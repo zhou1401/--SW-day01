1d084b2b5f8a558645fa4a5dd93ebcc60f31524a
jyz <jyz@jyzdeiMac.local>
2017-10-15 20:36:53 +0800

    Basic Code

---
+++ XL-Day01-SW/XL-Day01-SWTests/XL_Day01_SWTests.swift
@@ -0,0 +1,36 @@
+//
+//  XL_Day01_SWTests.swift
+//  XL-Day01-SWTests
+//
+//  Created by jyz on 2017/10/15.
+//  Copyright © 2017年 HDGS. All rights reserved.
+//
+
+import XCTest
+@testable import XL_Day01_SW
+
+class XL_Day01_SWTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
